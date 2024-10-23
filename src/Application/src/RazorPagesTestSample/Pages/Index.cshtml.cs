0d8b64d1dea9350e7b92bd76ac2ca0c1eb4adb4c
pfranco22 <pete22@hotmail.com>
2024-10-23 14:19:50 -0600

    Update Issue, Resolves #8

--- src/Application/src/RazorPagesTestSample/Pages/Index.cshtml.cs
+++ src/Application/src/RazorPagesTestSample/Pages/Index.cshtml.cs
@@ -11,7 +11,7 @@ using System.IO.Compression;
 namespace RazorPagesTestSample.Pages
 {
     public class IndexModel : PageModel
-    
+    {
         private readonly AppDbContext _db;
 
         public IndexModel(AppDbContext db)
